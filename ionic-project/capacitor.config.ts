80b9fe579b9bf7bba1d52ee2ee2ba600b7021e88
MarioCDS <101996644+MarioCDS@users.noreply.github.com>
2022-09-30 14:20:35 +0200

    Ionic starter app
    Creating the default ionic app.

---
+++ ionic-project/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-project',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
