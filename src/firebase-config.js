268f3196e82e6ad46d90091ac45afa3bc07bf4e2
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-10-17 11:20:51 +0200

    tried to read from firestore

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import "firebase/firestore";
 
 import firebase from "firebase/compat/app";
+
 import "firebase/compat/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 
-const app = firebase.initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = app.auth();
--- src/pages/Current.jsx
+++ src/pages/Current.jsx
@@ -4,11 +4,28 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonViewWillEnter,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Current.css";
+import db from "../firebase-config";
+import { useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const Current = () => {
+  const [songs, setSongs] = useState([]);
+
+  useIonViewWillEnter(() => {
+    fetchSongs();
+  }, []);
+
+  async function fetchSongs() {
+    const querySnapshot = await getDocs(collection(db, "Lyrics"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  }
+
   return (
     <IonPage>
       <IonHeader>
