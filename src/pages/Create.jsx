b04ce70409cd94a3bf1ec8f4c8980898105db3bd
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-10-18 12:05:49 +0200

    added editor

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -8,8 +8,12 @@ import {
 import "./Style.css";
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase-config";
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 export default function Create() {
+  const editorRef = useRef(null);
+
   async function handleCreate() {
     const docRef = doc(db, "Lyrics", "newSong2");
 
@@ -34,6 +38,42 @@ export default function Create() {
           </IonToolbar>
         </IonHeader>
         <button onClick={handleCreate}>Create test</button>
+        <Editor
+          tinymceScriptSrc={process.env.PUBLIC_URL + "/tinymce/tinymce.min.js"}
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            height: 500,
+
+            menubar: false,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "anchor",
+              "searchreplace",
+              "visualblocks",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "preview",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+        />
       </IonContent>
     </IonPage>
   );
