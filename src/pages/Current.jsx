75307b3772f2aebb6fd1a7d5f5b2817705e2517b
MarioCDS <101996644+MarioCDS@users.noreply.github.com>
2022-10-19 14:14:50 +0200

    Adding share functionality on the "Current" page

--- src/pages/Current.jsx
+++ src/pages/Current.jsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonCard,
   IonContent,
   IonHeader,
@@ -8,9 +9,18 @@ import {
 } from "@ionic/react";
 
 import { useAuth } from "../contexts/AuthContext";
+import { Share } from "@capacitor/share";
 import "./Style.css";
 
 const Current = () => {
+  async function handleShare() {
+    let lyrics = currentSong.Lyrics.replace(/<\/?[^>]+(>|$)/g, "");
+    await Share.share({
+      title: currentSong.Title,
+      text: lyrics,
+      Author: currentSong.Author,
+    });
+  }
   const { currentSong } = useAuth();
 
   return (
@@ -26,6 +36,9 @@ const Current = () => {
           <h1 className="title">{currentSong.Title}</h1>
           <div dangerouslySetInnerHTML={{ __html: currentSong.Lyrics }}></div>
         </IonCard>
+        <IonButton style={{ marginLeft: "47%" }} onClick={handleShare}>
+          Share
+        </IonButton>
       </IonContent>
     </IonPage>
   );
