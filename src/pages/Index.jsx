cd1389e7df8774e727efa1159685d6d552260f19
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-10-19 20:21:48 +0200

    Added confirmation dialog to delete in index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,27 +1,31 @@
 import {
+  IonButton,
+  IonCard,
   IonContent,
   IonHeader,
-  IonPage,
-  IonCard,
-  IonSearchbar,
-  useIonViewWillEnter,
   IonItem,
-  IonButton,
+  IonPage,
+  IonSearchbar,
+  useIonAlert,
+  useIonViewWillEnter,
 } from "@ionic/react";
-import "./Style.css";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { search, trashBin } from "ionicons/icons";
-import { db } from "../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
-import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
+import "./Style.css";
 
 function Search() {
   const [songs, setSongs] = useState([]);
   const historyUse = useHistory();
   const [filteredSongs, setFilteredSongs] = useState([]);
   const { setCurrentSong, setHistory, history, currentUser } = useAuth();
+  const [presentAlert] = useIonAlert();
+  const [handlerMessage, setHandlerMessage] = useState("");
+  const [roleMessage, setRoleMessage] = useState("");
+
   let songNumber = 1;
 
   useIonViewWillEnter(() => {
@@ -74,8 +78,29 @@ function Search() {
   };
 
   const handleDelete = async (song) => {
-    await deleteDoc(doc(db, "Lyrics", song.id));
-    fetchSongs();
+    await presentAlert({
+      header: "Are you sure you want to delete " + song.Title + "?",
+      buttons: [
+        {
+          text: "No",
+          role: "cancel",
+          handler: () => {
+            setHandlerMessage("Alert canceled");
+          },
+        },
+        {
+          text: "Yes",
+          role: "confirm",
+          handler: () => {
+            setHandlerMessage("Alert confirmed");
+            deleteDoc(doc(db, "Lyrics", song.id));
+            fetchSongs();
+          },
+        },
+      ],
+      onDidDismiss: (e) =>
+        setRoleMessage(`Dismissed with role: ${e.detail.role}`),
+    });
   };
 
   return (
@@ -114,6 +139,8 @@ function Search() {
               ) : null}
             </IonItem>
           ))}
+          <p>{handlerMessage}</p>
+          <p>{roleMessage}</p>
         </IonCard>
       </IonContent>
     </IonPage>
